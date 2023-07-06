b9dbc4836d944f37710051fafa6ffa70059a3473
Luke Waldron <lukewaldron87@gmail.com>
2023-07-06 16:05:29 +0200

    initial commit

---
+++ src/main/java/com/waldron/ecommerceservice/ECommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.waldron.ecommerceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waldron/ecommerceservice/ECommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldron.ecommerceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
