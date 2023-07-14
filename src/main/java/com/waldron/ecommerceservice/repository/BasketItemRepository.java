380c1fac5fe5201bbb2232b08221e442e42026ce
Luke Waldron <lukewaldron87@gmail.com>
2023-07-14 17:02:19 +0200

    added deleteByBasketId to BasketItemRepository

--- src/main/java/com/waldron/ecommerceservice/repository/BasketItemRepository.java
+++ src/main/java/com/waldron/ecommerceservice/repository/BasketItemRepository.java
@@ -4,11 +4,14 @@ import com.waldron.ecommerceservice.entity.BasketItem;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BasketItemRepository extends ReactiveCrudRepository<BasketItem, Long> {
 
     @Query("SELECT id, product_id, product_count, basket_id FROM basket_item WHERE basket_id = :basketId")
     Flux<BasketItem> findByBasketId(Long basketId);
 
-    //todo deleteBasketItemsForBasketId
+    @Query("DELETE FROM basket_item WHERE basket_id = :basketId")
+    Mono<Void> deleteByBasketId(Long id);
+
 }
--- src/test/java/com/waldron/ecommerceservice/repository/BasketItemRepositoryTest.java
+++ src/test/java/com/waldron/ecommerceservice/repository/BasketItemRepositoryTest.java
@@ -2,6 +2,8 @@ package com.waldron.ecommerceservice.repository;
 
 import com.waldron.ecommerceservice.entity.Basket;
 import com.waldron.ecommerceservice.entity.BasketItem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
@@ -20,32 +22,52 @@ class BasketItemRepositoryTest {
     @Autowired
     private BasketItemRepository basketItemRepository;
 
-    @Test
-    public void findByBasketId_shouldReturnAllBasketItemsWithGivenBasketId(){
+    private Basket basket1;
+    private Basket basket2;
+    private BasketItem basketItem1;
+    private BasketItem basketItem2;
+    private BasketItem basketItem3;
 
-        Basket basket1 = Basket.builder().build();
-        Basket basket2 = Basket.builder().build();
+    @BeforeEach
+    public void beforeEach(){
+
+        basket1 = Basket.builder().build();
+        basket2 = Basket.builder().build();
 
         basketRepository.saveAll(Arrays.asList(basket1, basket2)).log().subscribe();
 
-        BasketItem basketItem1 = BasketItem.builder()
+        basketItem1 = BasketItem.builder()
                 .basketId(basket1.getId())
                 .build();
 
-        BasketItem basketItem2 = BasketItem.builder()
+        basketItem2 = BasketItem.builder()
                 .basketId(basket1.getId())
                 .build();
 
         // this BasketItem has the wrong basketId and should not be returned
-        BasketItem basketItem3 = BasketItem.builder()
+        basketItem3 = BasketItem.builder()
                 .basketId(basket2.getId())
                 .build();
 
         basketItemRepository.saveAll(Arrays.asList(basketItem1, basketItem2, basketItem3)).log().subscribe();
 
+    }
+
+    @Test
+    public void findByBasketId_shouldReturnAllBasketItemsWithGivenBasketId(){
+
         StepVerifier.create(basketItemRepository.findByBasketId(basket1.getId()))
                 .expectNext(basketItem1)
                 .expectNext(basketItem2)
+                .verifyComplete();
+    }
+
+    @Test
+    public void deleteByBasketId_shouldDeleteAllBasketItemsWithGivenBasketId(){
+
+        basketItemRepository.deleteByBasketId(basket1.getId()).subscribe();
+
+        StepVerifier.create(basketItemRepository.findByBasketId(basket1.getId()))
                 .verifyComplete();
     }
 }
